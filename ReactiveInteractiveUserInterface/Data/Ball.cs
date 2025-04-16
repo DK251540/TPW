72e337e4e8808e6689b02583839033ca567ddd6e
Daniel Kałużny <251540@edu.p.lodz.pl>
2025-04-16 15:02:37 +0200

    Etap 1, usuniecie katalogu Etap0

---
+++ ReactiveInteractiveUserInterface/Data/Ball.cs
@@ -0,0 +1,76 @@
+﻿//____________________________________________________________________________________________________________________________________
+//
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//_____________________________________________________________________________________________________________________________________
+
+namespace TP.ConcurrentProgramming.Data
+{
+
+    internal class Ball : IBall
+    {
+        private readonly double BallDiameter; // <== Nowe pole
+
+        internal Ball(Vector initialPosition, Vector initialVelocity, double ballDiameters)
+        {
+            Position = initialPosition;
+            Velocity = initialVelocity;
+            BallDiameter = ballDiameters;
+        }
+        #region ctor
+
+        internal Ball(Vector initialPosition, Vector initialVelocity)
+        {
+            Position = initialPosition;
+            Velocity = initialVelocity;
+        }
+
+        #endregion ctor
+
+        #region IBall
+
+        public event EventHandler<IVector>? NewPositionNotification;
+
+        public IVector Velocity { get; set; }
+
+        #endregion IBall
+
+        #region private
+
+        public Vector Position;
+
+        private void RaiseNewPositionChangeNotification()
+        {
+            NewPositionNotification?.Invoke(this, Position);
+        }
+
+        internal void Move(Vector delta)
+        {
+            double newX = Position.x + delta.x;
+            double newY = Position.y + delta.y;
+            double radius = BallDiameter / 2.0;
+
+            // Odbicia od ścian z uwzględnieniem promienia
+            if (newX - radius < 0)
+                newX = radius;
+            else if (newX + radius > 400)
+                newX = 400 - radius;
+
+            if (newY - radius < 0)
+                newY = radius;
+            else if (newY + radius > 400)
+                newY = 400 - radius;
+
+            Position = new Vector(newX, newY);
+            RaiseNewPositionChangeNotification();
+        }
+
+        #endregion private
+    }
+
+
+}
---
+++ ReactiveInteractiveUserInterface/Data/DataImplementation.cs
@@ -0,0 +1,130 @@
+﻿//____________________________________________________________________________________________________________________________________
+//
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//_____________________________________________________________________________________________________________________________________
+
+using System;
+using System.Diagnostics;
+
+namespace TP.ConcurrentProgramming.Data
+{
+    internal class DataImplementation : DataAbstractAPI
+    {
+        private readonly double BallDiameter = 20.0;
+        private readonly double TableWidth = 400.0;
+        private readonly double TableHeight = 400.0;
+        #region ctor
+
+        public DataImplementation()
+        {
+            MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+        }
+
+        #endregion ctor
+
+        #region DataAbstractAPI
+
+        public override void Start(int numberOfBalls, Action<IVector, IBall> upperLayerHandler)
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(DataImplementation));
+            if (upperLayerHandler == null)
+                throw new ArgumentNullException(nameof(upperLayerHandler));
+            Random random = new Random();
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                Vector startingPosition = new(random.Next(100, 400 - 100), random.Next(100, 400 - 100));
+                Ball newBall = new(startingPosition, startingPosition, BallDiameter);
+                upperLayerHandler(startingPosition, newBall);
+                BallsList.Add(newBall);
+            }
+        }
+
+        #endregion DataAbstractAPI
+
+        #region IDisposable
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    MoveTimer.Dispose();
+                    BallsList.Clear();
+                }
+                Disposed = true;
+            }
+            else
+                throw new ObjectDisposedException(nameof(DataImplementation));
+        }
+
+        public override void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable
+
+        #region private
+
+        //private bool disposedValue;
+        private bool Disposed = false;
+
+        private readonly Timer MoveTimer;
+        private Random RandomGenerator = new();
+        private List<Ball> BallsList = [];
+
+        private void Move(object? x)
+        {
+            foreach (Ball item in BallsList)
+            {
+                // Losowe przesunięcie
+                double deltaX = (RandomGenerator.NextDouble() - 0.5) * 10;
+                double deltaY = (RandomGenerator.NextDouble() - 0.5) * 10;
+
+                double newX = item.Position.x + deltaX;
+                double newY = item.Position.y + deltaY;
+
+                // Ograniczenia pozycji, żeby piłka nie wyszła poza stół
+                newX = Math.Max(0, Math.Min(newX, TableWidth - BallDiameter - 5));
+                newY = Math.Max(0, Math.Min(newY, TableHeight - BallDiameter - 5));
+
+                // Przekazujemy rzeczywisty wektor przesunięcia
+                item.Move(new Vector(newX - item.Position.x, newY - item.Position.y));
+            }
+        }
+
+
+        #endregion private
+
+        #region TestingInfrastructure
+
+        [Conditional("DEBUG")]
+        internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
+        {
+            returnBallsList(BallsList);
+        }
+
+        [Conditional("DEBUG")]
+        internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
+        {
+            returnNumberOfBalls(BallsList.Count);
+        }
+
+        [Conditional("DEBUG")]
+        internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
+        {
+            returnInstanceDisposed(Disposed);
+        }
+
+        #endregion TestingInfrastructure
+    }
+}
